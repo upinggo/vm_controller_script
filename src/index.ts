bb87416532652288127f03da6f27e0b2d75d7b55
Neil Shen <neil.shen@sap.com>
2025-06-11 12:03:30 +0800

    Enhance Cloud Foundry login handling with interactive input and improved error management

--- src/index.ts
+++ src/index.ts
@@ -17,29 +17,68 @@ const argv = minimist(process.argv.slice(2));
 const branchName = argv.b || argv._[0] || process.env.BRANCH || 'main';
 console.log(`Branch name: ${branchName}`);
 const commands = [
+  `cd ${process.env.REPO_PATH} && git fetch --all && git checkout ${branchName} && git reset --hard origin/${branchName} && git pull`,
   `npm --prefix ${process.env.REPO_PATH} install`,
   `npm --prefix ${process.env.REPO_PATH} run deploy:umbrella ${process.env.HANA_CONTAINER} ${process.env.OPTIONS ? '-- ' + process.env.OPTIONS : ''}`,
 ];
 const conn = new Client();
+function connectCFShell(conn: Client) {
+  let cfLoginCommand = process.env.CFLoginCommand || 'cf login';
+  conn.shell((err, stream) => {
+    if (err) {
+      console.error('Error starting shell:', err);
+      return;
+    }
+
+    stream.write('cf oauth-token\n',
+      () => {
+        stream.stdout.on('data', (data: any) => {
+          console.log('Checking Cloud Foundry login status..., data:', data.toString());
+          if (data.toString().includes('FAILED')) {
+            console.warn('Not logged in to Cloud Foundry, executing login command...');
+            stream.write(cfLoginCommand + '\n', () => {
+              // Enable raw mode for interactive input
+              process.stdin.setRawMode(true);
+              //  process.stdin.resume();
+
+              let inputBuffer = '';
+
+              // Handle user input
+              process.stdin.on('data', (data: Buffer) => {
+                const input = data.toString();
+                // Check for termination conditions
+                if (input === '\u0004') { // Ctrl+D
+                  console.log(`Sending command: ${inputBuffer}`);
+                  stream.write(inputBuffer + '\n');
+                  inputBuffer = '';
+                  stream.pipe(stream.stdout); // Pipe stream output to stdout
+                  stream.close()
+                } else {
+                  inputBuffer += input;
+                }
+              });
+
+
+              // Handle shell closure
+              stream.on('close', () => {
+                console.log('Shell session closed');
+                process.stdin.setRawMode(false); // Disable raw mode
+                //  process.stdin.pause(); // Stop listening for input
+              }).on('data', (data: any) => {
+                console.log(`Shell STDOUT: ${data}`);
+              });
+            });
+          }
+        });
+      }
+    );
+  });
+}
 
 conn.on('ready', () => {
   console.log('Client :: ready');
-
-  console.log('Checking Cloud Foundry login status...');
-  try {
-    execSync(`cd ${process.env.REPO_PATH} && git fetch --all && git checkout ${branchName} && git reset --hard origin/${branchName}`, { stdio: "ignore" });
-    execSync("cf oauth-token", { stdio: "ignore" });
-  } catch (error) {
-    try {
-      execSync(process.env.CFLoginCommand as string, { stdio: "inherit" });
-      // Re-check login status after attempting login
-      execSync("cf oauth-token", { stdio: "ignore" });
-    } catch (loginError) {
-      process.exit(1);
-    }
-  }
+  connectCFShell(conn);
   executeCommands(conn, commands);
-
 }).on('error', (err) => {
   console.error('Connection Error:', err);
 }).connect(connectionConfig);
