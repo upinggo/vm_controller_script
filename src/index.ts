9a5c1af86398256da41895546c179008aecc3bed
Neil Shen <neil.shen@sap.com>
2025-06-17 13:44:38 +0800

    Refactor Cloud Foundry shell connection to support interactive login and improve error handling

--- src/index.ts
+++ src/index.ts
@@ -22,62 +22,71 @@ const commands = [
   `npm --prefix ${process.env.REPO_PATH} run deploy:umbrella ${process.env.HANA_CONTAINER} ${process.env.OPTIONS ? '-- ' + process.env.OPTIONS : ''}`,
 ];
 const conn = new Client();
-function connectCFShell(conn: Client) {
-  let cfLoginCommand = process.env.CFLoginCommand || 'cf login';
-  conn.shell((err, stream) => {
-    if (err) {
-      console.error('Error starting shell:', err);
-      return;
-    }
-
-    stream.write('cf oauth-token\n',
-      () => {
-        stream.stdout.on('data', (data: any) => {
-          console.log('Checking Cloud Foundry login status..., data:', data.toString());
-          if (data.toString().includes('FAILED')) {
-            console.warn('Not logged in to Cloud Foundry, executing login command...');
-            stream.write(cfLoginCommand + '\n', () => {
-              // Enable raw mode for interactive input
-              process.stdin.setRawMode(true);
-              //  process.stdin.resume();
-
-              let inputBuffer = '';
-
-              // Handle user input
-              process.stdin.on('data', (data: Buffer) => {
-                const input = data.toString();
-                // Check for termination conditions
-                if (input === '\u0004') { // Ctrl+D
-                  console.log(`Sending command: ${inputBuffer}`);
-                  stream.write(inputBuffer + '\n');
-                  inputBuffer = '';
-                  stream.pipe(stream.stdout); // Pipe stream output to stdout
-                  stream.close()
-                } else {
-                  inputBuffer += input;
-                }
-              });
-
-
-              // Handle shell closure
-              stream.on('close', () => {
-                console.log('Shell session closed');
-                process.stdin.setRawMode(false); // Disable raw mode
-                //  process.stdin.pause(); // Stop listening for input
-              }).on('data', (data: any) => {
-                console.log(`Shell STDOUT: ${data}`);
-              });
-            });
-          }
-        });
+function connectCFShell(conn: Client): Promise<void> {
+  return new Promise((resolve, reject) => {
+    let cfLoginCommand = process.env.CFLoginCommand || 'cf login';
+    conn.shell((err, stream) => {
+      if (err) {
+        console.error('Error starting shell:', err);
+        reject(err);
+        return;
       }
+
+      stream.write('cf oauth-token\n',
+        () => {
+          stream.stdout.on('data', (data: any) => {
+            console.log('Checking Cloud Foundry login status..., data:', data.toString());
+            if (data.toString().includes('FAILED')) {
+              console.warn('Not logged in to Cloud Foundry, executing login command...');
+              stream.write(cfLoginCommand + '\n', () => {
+                // Enable raw mode for interactive input
+                process.stdin.setRawMode(true);
+                //  process.stdin.resume();
+
+                let inputBuffer = '';
+
+                // Handle user input
+                process.stdin.on('data', (data: Buffer) => {
+                  const input = data.toString();
+                  // Check for termination conditions
+                  if (input === '\u0004') { // Ctrl+D
+                    console.log(`Sending command: ${inputBuffer}`);
+                    stream.write(inputBuffer + '\n');
+                    inputBuffer = '';
+                    stream.pipe(stream.stdout); // Pipe stream output to stdout
+                    resolve();
+                    stream.close()
+                  } else {
+                    inputBuffer += input;
+                  }
+                });
+
+
+              });
+            } else if (data.toString().includes('bearer')) {
+              console.log('Already logged in to Cloud Foundry');
+              resolve();
+              stream.close();
+            }
+          }
+          );
+          // Handle shell closure
+          stream.on('close', () => {
+            console.log('Shell session closed');
+            process.stdin.setRawMode(false); // Disable raw mode
+            //  process.stdin.pause(); // Stop listening for input
+          }).on('data', (data: any) => {
+            console.log(`Shell STDOUT: ${data}`);
+          });
+        });
+    }
     );
-  });
+  })
 }
 
-conn.on('ready', () => {
+conn.on('ready', async () => {
   console.log('Client :: ready');
-  connectCFShell(conn);
+  await connectCFShell(conn);
   executeCommands(conn, commands);
 }).on('error', (err) => {
   console.error('Connection Error:', err);
