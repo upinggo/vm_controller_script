a6b76ce1de013934d36df1d8046e20a7e96b2eb9
Neil Shen <neil.shen@sap.com>
2025-06-23 10:12:30 +0800

    add parser

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,7 @@ function connectCFShell(conn: Client): Promise<void> {
                 process.stdin.on('data', (data: Buffer) => {
                   const input = data.toString();
                   // Check for termination conditions
-                  if (input === '\u0004') { // Ctrl+D
+                  if (input === '\\n') { // \n
                     console.log(`Sending command: ${inputBuffer}`);
                     stream.write(inputBuffer + '\n');
                     inputBuffer = '';
